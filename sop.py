62ff055fa003b8972fe7e9763908e5e5233e6bdd
Ryan Fox <foxryan@gmail.com>
2022-11-25 20:14:03 -0500

    Add note about item locking before getting started

--- sop.py
+++ sop.py
@@ -12,12 +12,22 @@ def diff(a: str, b: str) -> str:
 
 
 def main() -> None:
-    print(
-        'Please ensure that Stranger of Paradise: Final Fantasy Origin is running and you have loaded your save.'
-    )
-    input('(continue)')
+    print('''
+Please ensure that Stranger of Paradise: Final Fantasy Origin is running and you have loaded your save.
+
+***********************************************************************************
+* NOTE: All items will be unlocked, and then the items to be kept will be locked. *
+* If you don't want your locked items to be changed, press Ctrl+C to cancel now.  *
+***********************************************************************************
+''')
+    try:
+        input('(continue)')
+    except EOFError:
+        print('\nCancelled.')
+        return
 
     inv = Inventory.from_process()
+    
     print('Unlocking all items...')
     for item in inv.items:
         item.locked = False
@@ -27,7 +37,7 @@ def main() -> None:
         item.locked = True
 
     print('You can now dismantle all unlocked items from within the game.')
-    input('(continue)')
+    input('(done)')
 
 
 if __name__ == '__main__':
