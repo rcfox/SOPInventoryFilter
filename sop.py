4bc1d42371946bbd273baf1b9c8c6edcdf8fc4a9
Ryan Fox <foxryan@gmail.com>
2023-01-28 01:44:54 -0500

    Add commands for unlocking all items and finding upgrades

--- sop.py
+++ sop.py
@@ -1,9 +1,11 @@
 from memory import Inventory, Item, Effect
 from collections import defaultdict
-from database import Database, Strings
+from database import Database, Strings, ItemsDB
 from pathlib import Path
 import csv
 import sqlite3
+import click
+
 
 def diff(a: str, b: str) -> str:
     result = []
@@ -42,8 +44,71 @@ def create_db(inventory):
                 item.insert_row(conn)
     finally:
         conn.close()
+        
+@click.group()
+def main():
+    pass
+        
+@main.command()
+def unlock_all() -> None:
+    print('Unlocking all items.')
+    inv = Inventory.from_process()
+    for item in inv.items:
+        item.locked = False
+        
+@main.command()
+def upgrades() -> None:   
+    inv = Inventory.from_process()#Inventory.from_file(Path('inv.bin'))
+    statuses = defaultdict(list)
+    
+    item_types = defaultdict(list)
+    upgrade = []
 
-def main() -> None:   
+    for item in inv.items:
+        if item.item_id == 0:
+            continue
+        item_types[item.type].append(item)
+        if ItemsDB[item.item_id].slots == '2-Slot Armour':
+            item_types['Head'].append(item)
+            item_types['Leg'].append(item)
+        if item.locked and not bool(item.status & 1):
+            upgrade.append(item)
+            
+    possible_upgrades = []
+    for upgrade_item in upgrade:
+        upgrade_item.locked = False
+        for item in item_types[upgrade_item.type]:
+            if item.locked:
+                continue
+            for upgrade_eff in upgrade_item.effects:
+                for item_eff in item.effects:
+                    if upgrade_eff.effect_id == item_eff.effect_id:
+                        if upgrade_item.type == 'Accessory':
+                            if item_eff.raw_amount > upgrade_eff.raw_amount:
+                                possible_upgrades.append((upgrade_item, upgrade_eff, item, item_eff))
+                        else:
+                            if item_eff.affinity_level > upgrade_eff.affinity_level:
+                                possible_upgrades.append((upgrade_item, upgrade_eff, item, item_eff))
+                                
+    results = defaultdict(lambda: defaultdict(list))
+    for upgrade_item, upgrade_eff, item, item_eff in possible_upgrades:
+        results[f'{upgrade_item.name} (lvl{upgrade_item.level})'][repr(upgrade_eff)].append((item, item_eff))
+        
+    for upgrade_item, effects in results.items():
+        print(upgrade_item)
+        for effect, items in effects.items():
+            print('--', effect)
+            for item, item_eff in sorted(items, key=lambda i: repr(i[1]), reverse=True):
+                item.locked = True
+                print(f'---- {item.name} (lvl{item.level}) - {repr(item_eff)}')
+                
+    #for upgrade_item, upgrade_eff, item, item_eff in possible_upgrades:
+    #    item.locked = True
+    #    print(f'{upgrade_item.name} (lvl{upgrade_item.level}) - {repr(upgrade_eff)} <-- {item.name} (lvl{item.level}) - {repr(item_eff)}')
+
+
+@main.command()
+def filter_inventory() -> None:   
     
     print('''
 Please ensure that Stranger of Paradise: Final Fantasy Origin is running and you have loaded your save.
@@ -70,6 +135,7 @@ Please ensure that Stranger of Paradise: Final Fantasy Origin is running and you
     for item in inv.filter():
         item.locked = True
         
+    inv.save(Path('inv.bin'))
     create_db(inv)
 
     print('You can now dismantle all unlocked items from within the game.')
